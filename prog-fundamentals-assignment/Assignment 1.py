62bae92e8a81b8e152185657fdcd663942ff3122
Dhsnyc <dharmasudibya@gmail.com>
2025-09-02 11:08:04 +0700

    Add files via upload

---
+++ prog-fundamentals-assignment/Assignment 1.py
@@ -0,0 +1,11 @@
+#Kode 1
+length = 5
+width = 3
+area = length * width
+print("Versi 1: Luas persegi panjang =", area)
+
+#Kode 2
+length = int(input("Masukkan panjang: "))
+width = int(input("Masukkan lebar: "))
+area = length * width
+print("Versi 2: Luas persegi panjang =", area)
